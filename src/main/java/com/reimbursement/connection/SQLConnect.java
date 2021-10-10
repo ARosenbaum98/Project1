b6b1f151950415f45b3f66f472fd2df56c3da4a8
ARosenbaum98 <90282324+ARosenbaum98@users.noreply.github.com>
2021-10-10 15:45:41 -0500

    JPA Conversion Failure

--- src/main/java/com/reimbursement/connection/SQLConnect.java
+++ src/main/java/com/reimbursement/connection/SQLConnect.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.jdbc.Work;
 import org.hibernate.query.Query;
 
